b7da5663c993c6a3f229ac78028bf6f36f145179
Noel Pascal Schwabenland <noel@schwabenland.ch>
2023-05-19 18:00:52 +0200

    added token counter

--- src/openai.ts
+++ src/openai.ts
@@ -1,5 +1,10 @@
+import { config } from 'dotenv';
+import { encode } from 'gpt-3-encoder';
 import { Configuration, OpenAIApi } from 'openai';
 
+
+/** Only use .env files when running in dev mode */
+if (!process.env.produtction) config();
 
 /** Openai */
 export const openaiConfiguration = new Configuration({
@@ -7,3 +12,8 @@ export const openaiConfiguration = new Configuration({
 });
 export const openai = new OpenAIApi(openaiConfiguration);
 
+/** Utility to manage tokens */
+export const countTokens = (text: string | string[]) => {
+    if (Array.isArray(text)) return encode(text.join(' ')).length;
+    return encode(text).length;
+};
